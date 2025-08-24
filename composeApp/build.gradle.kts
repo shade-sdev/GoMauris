bc06aeec531bd96adda39e1ac70d019077ccdf42
Shade <shade@shade.com>
2025-08-24 02:43:21 -0700

    ffs

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,5 +1,6 @@
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 import java.net.URI
 
 plugins {
@@ -29,19 +30,15 @@ kotlin {
         iosArm64(),
         iosSimulatorArm64()
     ).forEach { iosTarget ->
-        iosTarget.compilations {
-            val main by getting {
-                cinterops.create("maplibreRops")
-            }
-        }
         iosTarget.binaries.framework {
             baseName = "ComposeApp"
             isStatic = true
         }
+        iosTarget.configureSpmMaplibre(project)
     }
 
     swiftPackageConfig {
-        create("maplibreRops") {
+        create("spmMaplibre") {
             dependency {
                 remotePackageVersion(
                     url = URI("https://github.com/maplibre/maplibre-gl-native-distribution.git"),
@@ -61,13 +58,11 @@ kotlin {
             implementation(compose.runtime)
             implementation(compose.foundation)
             implementation(compose.material3)
+            implementation(compose.material)
             implementation(compose.ui)
             implementation(compose.materialIconsExtended)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
-
-            implementation(libs.androidx.lifecycle.viewmodelCompose)
-            implementation(libs.androidx.lifecycle.runtimeCompose)
 
             implementation(libs.maplibre.composeMaterial3)
 
@@ -123,3 +118,18 @@ dependencies {
     debugImplementation(compose.uiTooling)
 }
 
+fun KotlinNativeTarget.configureSpmMaplibre(project: Project) {
+    // ideally the SPM gradle plugin should handle this for us
+    val variant =
+        when (targetName) {
+            "iosArm64" -> "arm64-apple-ios"
+            "iosSimulatorArm64" -> "arm64-apple-ios-simulator"
+            "iosX64" -> "x86_64-apple-ios-simulator"
+            else -> error("Unrecognized target: $targetName")
+        }
+    print(project.layout.buildDirectory)
+    val rpath =
+        "${project.layout.buildDirectory.get()}/spmKmpPlugin/spmMaplibre/scratch/$variant/release/"
+    binaries.all { linkerOpts("-F$rpath", "-rpath", rpath) }
+    compilations.getByName("main") { cinterops { create("spmMaplibre") } }
+}
