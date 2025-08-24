567c4f2c7f04c076d2b776b7723a9c1e650dced8
Shade <Shade@Shade.ga>
2025-08-24 10:48:48 +0400

    Initial Commit

--- composeApp/src/commonMain/kotlin/dev/shade/gomauris/ui/screen/tabs/HomeTab.kt
+++ composeApp/src/commonMain/kotlin/dev/shade/gomauris/ui/screen/tabs/HomeTab.kt
@@ -1,35 +1,33 @@
 package dev.shade.gomauris.ui.screen.tabs
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
-import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.navigator.tab.Tab
 import cafe.adriel.voyager.navigator.tab.TabOptions
+import dev.shade.gomauris.App
+import io.github.dellisd.spatialk.geojson.Position
 
 object HomeTab : Tab {
 
     @Composable
     override fun Content() {
-        LazyColumn(
-            modifier = Modifier.fillMaxSize()
-        ) {
-            items(30) { index ->
-                Text(
-                    text = "Item #$index",
-                    color = Color.Black,
-                    modifier = Modifier.padding(16.dp)
+        var destination by remember {
+            mutableStateOf(
+                Position(
+                    latitude = -20.1669,
+                    longitude = 57.5023
                 )
-            }
+            )
         }
+        val source = Position(latitude = -20.24444, longitude = 57.55417)
+        var routeCoordinates by remember { mutableStateOf<List<Position>>(emptyList()) }
+        App(routeCoordinates, source, destination)
     }
 
     override val options: TabOptions
